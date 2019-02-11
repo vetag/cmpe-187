50de74c5bf2916bb9c6ee423131ba985f68ac161
richsensations <32314978+richsensations@users.noreply.github.com>
2019-02-10 17:42:16 -0800

    Added test cases and fixed bug in calculations

--- src/writingtestcases/WritingTestCases.java
+++ src/writingtestcases/WritingTestCases.java
@@ -3,27 +3,29 @@ package writingtestcases;
 public class WritingTestCases {
 
 	/*
-	 * Constructor for class, ints not used
-	 */
-	public WritingTestCases() {
-		int x;
-		int y;
-		int z;
-	}
-
-	/*
 	 * Tests if the numbers given form a triangle or not. If so, returns if the triangle is equilateral, isosceles, or scalar with a boolean of true.
 	 * If numbers do not form a triangle, returns that they do not form a triangle and a boolean of false.
 	 * @param x the x side of triangle
 	 * @param y the y side of triangle
 	 * @param z the z side of triangle
+	 * 
+	 * 
+	 * Uses the triangle inequality theorem which states that the sum of the side lengths of any 2 sides 
+	 * of a triangle must exceed the length of the third side.
 	 */
 	public boolean TriangleOrNot(int x, int y, int z) {
+		
 		boolean test = false;
+		
+		if(x <= 0 || y <= 0 || z <= 0){
+			System.out.println("Invalid values(less than one)");
+			return test;
+		}
+		
 		int xySides = x + y;
 		int zySides = z + y;
 		int zxSides = z + x;
-		if(xySides > z && zySides > x && zxSides > y || x > 0 || y > 0 || z > 0) {
+		if((xySides > z && zySides > x && zxSides > y)) {
 			if(x == y && x == z) {
 				System.out.println("The triangle is equilateral");
 				test = true;
@@ -42,30 +44,28 @@ public class WritingTestCases {
 		}
 		return test;
 	}
-
+	
 	/*
 	 * Tester method
 	 * Change the numbers inside TriangleOrNot() to test triangles. First number is x, second number is y, third number is z
 	 */
 	public static void main(String[] args) {
-		System.out.println(new WritingTestCases().TriangleOrNot(11, 1, 12));
-
 		System.out.println("\nTesting with x=1, y=1, z=1:");
 		System.out.println(new WritingTestCases().TriangleOrNot(1, 1, 1));
-
-		System.out.println("\nTesting with x=2, y=2, z=4:");
-		System.out.println(new WritingTestCases().TriangleOrNot(2, 2, 4));
-
-		System.out.println("\nTesting with x=1, y=2, z=3:");
-		System.out.println(new WritingTestCases().TriangleOrNot(1, 2, 3));
-
+		
+		System.out.println("\nTesting with x=9, y=9, z=14:");
+		System.out.println(new WritingTestCases().TriangleOrNot(9, 9, 14));
+		
+		System.out.println("\nTesting with x=6, y=12, z=14:");
+		System.out.println(new WritingTestCases().TriangleOrNot(6, 12, 14));
+		
 		System.out.println("\nTesting with x=0, y=0, z=0:");
 		System.out.println(new WritingTestCases().TriangleOrNot(0, 0, 0));
-
-		System.out.println("\nTesting with x=-1, y=0, z=0:");
-		System.out.println(new WritingTestCases().TriangleOrNot(-1, 0, 0));
 		
-		System.out.println("\nTesting with x=x, y=y, z=z:");
-		System.out.println(new WritingTestCases().TriangleOrNot(x, y, z));
+		System.out.println("\nTesting with x=-1, y=-1, z=-1:");
+		System.out.println(new WritingTestCases().TriangleOrNot(-1, -1, -1));
+		
+		System.out.println("\nTesting with x=2, y=3, z=10:");
+		System.out.println(new WritingTestCases().TriangleOrNot(2, 3, 10));
 	}
 }
